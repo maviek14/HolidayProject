9a628b4108af88d26be8f4ac203b38b02a586434
maviek14 <mt82032@stud.uph.edu.pl>
2023-12-06 16:15:49 +0100

    Added DateRangeViewComponent and date picking logic

---
+++ HolidayProject/Models/DateRangePickerViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace HolidayProject.Models;
+
+public class DateRangePickerViewModel
+{
+    public DateTime? Start { get; set; }
+    public DateTime? End { get; set; }
+}
---
+++ HolidayProject/ViewComponents/DateRangeViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using HolidayProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayProject.ViewComponents;
+
+public class DateRangeViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(DateRangePickerViewModel model)
+    {
+        return View(model);
+    }
+}
