b5d0b065ad8ad322c2f13227acdff11d5805ad36
maviek14 <mt82032@stud.uph.edu.pl>
2023-12-06 13:00:20 +0100

    Added PropertyDetailsModel, updated data

--- HolidayProject/Controllers/PropertyListingController.cs
+++ HolidayProject/Controllers/PropertyListingController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace HolidayProject.Controllers;
 public class PropertyListingController : Controller
 {
-    private static List<PropertyListingModel> properties = new()
+    private static List<PropertyDetailsModel> properties = new()
     {
         new()
         {
@@ -13,7 +13,15 @@ public class PropertyListingController : Controller
             Blurb = "Beautiful cottage on the Cornwall coast",
             Location = "Cornwall",
             NumberOfBedrooms = 3,
-            CostPerNight = 350
+            CostPerNight = 350,
+            Description = "Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Free Wi-Fi", "Fully Equipped Kitchen" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2024-01-04"),
+                DateTime.Parse("2024-01-05"),
+                DateTime.Parse("2024-01-06")
+            }
         },
 
         new()
@@ -23,7 +31,15 @@ public class PropertyListingController : Controller
             Blurb = "Stately home on the Devon moores",
             Location = "Devon",
             NumberOfBedrooms = 7,
-            CostPerNight = 730
+            CostPerNight = 730,
+            Description="Enjoy the tranquility of the forest, with hiking trails right at your doorstep",
+            Amenities = new(){ "Wine Cellar", "Balinese Decor" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2023-12-04"),
+                DateTime.Parse("2023-12-05"),
+                DateTime.Parse("2023-12-06")
+            }
         },
 
         new()
@@ -33,7 +49,16 @@ public class PropertyListingController : Controller
             Blurb = "Experience coastal living at its finest in this charming 2-bedroom beachfront cottage.",
             Location = "Malibu",
             NumberOfBedrooms = 2,
-            CostPerNight = 250
+            CostPerNight = 250,
+            Description="Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Private Beach Access", "Oceanfront Deck", "Fully Equipped Kitchen", "Free Wi-Fi" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2023-12-15"),
+                DateTime.Parse("2023-12-16"),
+                DateTime.Parse("2023-12-17"),
+                DateTime.Parse("2023-12-18")
+            }
         },
 
         new()
@@ -43,7 +68,16 @@ public class PropertyListingController : Controller
             Blurb = "Indulge in luxury at this sleek and modern 3-bedroom penthouse with panoramic city views.",
             Location = "New York City",
             NumberOfBedrooms = 3,
-            CostPerNight = 500
+            CostPerNight = 500,
+            Description="Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Skyline View Terrace", "High-End Kitchen Appliances", "Smart Home Technology", "Fitness Center Access" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2023-12-21"),
+                DateTime.Parse("2023-12-22"),
+                DateTime.Parse("2024-02-17"),
+                DateTime.Parse("2024-02-18")
+            }
         },
 
         new()
@@ -53,7 +87,16 @@ public class PropertyListingController : Controller
             Blurb = "Escape to the mountains and unwind in this cozy 1-bedroom chalet surrounded by nature.",
             Location = "Aspen",
             NumberOfBedrooms = 1,
-            CostPerNight = 150
+            CostPerNight = 150,
+            Description="Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Private Forest Trails", "Fireplace", "Outdoor Hot Tub", "Wi-Fi" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2024-03-04"),
+                DateTime.Parse("2024-03-05"),
+                DateTime.Parse("2024-03-06"),
+                DateTime.Parse("2024-03-07")
+            }
         },
 
         new()
@@ -63,7 +106,16 @@ public class PropertyListingController : Controller
             Blurb = "Immerse yourself in history at this elegant 4-bedroom villa, nestled in the heart of an ancient European town.",
             Location = "Tuscany",
             NumberOfBedrooms = 4,
-            CostPerNight = 300
+            CostPerNight = 300,
+            Description="Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Private Forest Trails", "Fireplace", "Outdoor Hot Tub", "Wi-Fi" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2024-03-04"),
+                DateTime.Parse("2024-03-05"),
+                DateTime.Parse("2024-03-06"),
+                DateTime.Parse("2024-03-07")
+            }
         },
 
         new()
@@ -73,7 +125,16 @@ public class PropertyListingController : Controller
             Blurb = "Unwind in this tropical 2-bedroom bungalow surrounded by lush gardens and just steps away from the beach.",
             Location = "Bali",
             NumberOfBedrooms = 2,
-            CostPerNight = 200
+            CostPerNight = 200,
+            Description="Wake up to the sound of waves and enjoy breathtaking ocean views from your private deck",
+            Amenities = new(){ "Private Forest Trails", "Fireplace", "Outdoor Hot Tub", "Wi-Fi" },
+            BookedDates = new()
+            {
+                DateTime.Parse("2024-03-04"),
+                DateTime.Parse("2024-03-05"),
+                DateTime.Parse("2024-03-06"),
+                DateTime.Parse("2024-03-07")
+            }
         }
     };
 
@@ -85,5 +146,12 @@ public class PropertyListingController : Controller
     public IActionResult ListAvailable(DateTime start, DateTime end)
     {
         return View(properties);
+    }
+
+    public IActionResult ViewPropertyDetails(int id)
+    {
+        PropertyListingModel model = properties.FirstOrDefault(x => x.Id == id);
+
+        return View(model);
     }
 }
---
+++ HolidayProject/Models/PropertyDetailsModel.cs
@@ -0,0 +1,8 @@
+﻿namespace HolidayProject.Models;
+
+public class PropertyDetailsModel : PropertyListingModel
+{
+    public string Description { get; set; }
+    public List<string> Amenities { get; set; }
+    public List<DateTime> BookedDates { get; set; }
+}
