0b133e0600807e57e1ec3004731b76babcf55fe3
maviek14 <mt82032@stud.uph.edu.pl>
2023-12-07 10:47:17 +0100

    Added creating new property logic

---
+++ HolidayProject/Controllers/PropertyManagementController.cs
@@ -0,0 +1,22 @@
+﻿using HolidayProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HolidayProject.Controllers;
+
+public class PropertyManagementController : Controller
+{
+    [HttpGet]
+    public IActionResult AddProperty()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult AddProperty(PropertyDetailsModel model)
+    {
+       if (ModelState.IsValid)
+            return View("PropertyAdded", model);
+       else
+            return View(model);
+    }
+}
